0679f5c8145fc057d18ac59e5112541016fc3aba
ben watt <ben.watt@sorted.com>
2021-06-07 21:14:20 +0100

    Create terraform file to spin up an instance of service bus to test with

---
+++ infastructure/main.tf
@@ -0,0 +1,39 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "31bbb20e-79db-4cc6-a446-c1037b5539cb"
+}
+
+resource "azurerm_resource_group" "messaging-sidecar" {
+  name     = "messaging-sidecar"
+  location = "West Europe"
+}
+
+resource "azurerm_servicebus_namespace" "messaging-sidecar" {
+  name                = "messaging-sidecar-servicebus-ns"
+  location            = azurerm_resource_group.messaging-sidecar.location
+  resource_group_name = azurerm_resource_group.messaging-sidecar.name
+  sku                 = "Standard"
+
+  tags = {
+    source = "terraform"
+  }
+}
+
+resource "azurerm_servicebus_topic" "messaging-sidecar" {
+  name                = "messaging-sidecar-topic"
+  resource_group_name = azurerm_resource_group.messaging-sidecar.name
+  namespace_name      = azurerm_servicebus_namespace.messaging-sidecar.name
+  enable_partitioning = true
+}
