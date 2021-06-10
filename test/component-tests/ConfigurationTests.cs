18f3dc38ae369004e9aea0eee76d1d17aa6abce2
ben watt <ben.watt@sorted.com>
2021-06-10 21:58:41 +0100

    Add basic configuration tests

--- test/component-tests/ConfigurationTests.cs
+++ test/component-tests/ConfigurationTests.cs
@@ -1,12 +1,38 @@
+using messaging_sidecar.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using Xunit;
+
 namespace component_tests 
 {
-    public class ConfigurationTests 
+    public class ConfigurationTests : IClassFixture<CustomWebApplicationFactory>
     {
-        // Create tests to check our config
-        // Example config
+        private readonly CustomWebApplicationFactory _fixture;
 
-        public void SampleConfig() 
+        public ConfigurationTests(CustomWebApplicationFactory fixture)
         {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void When_a_yaml_file_is_specified_it_is_parsed_into_config()
+        {
+            _fixture.AddYamlFile("sample-config.yaml");
+            _fixture.CreateClient();
+
+            var config = _fixture.Services.GetService<IConfiguration>();
+
+            var version = config.GetValue<string>("version");
+            foreach (var provider in config.GetSection("message_providers").GetChildren())
+            {
+                if(provider.GetValue<string>("type") == "service_bus")
+                {
+                    // ToDo: Create and Bind to ServiceBusProviderOption
+                }
+            }
+
+            Assert.Equal("0.1", version);
         }
     }
 }
---
+++ test/component-tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,30 @@
+﻿using messaging_sidecar;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection;
+
+namespace component_tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        private string _yamlFilePath;
+
+        public void AddYamlFile(string path)
+        {
+            _yamlFilePath = path;
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(configBuilder =>
+            {
+                configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                configBuilder.AddYamlFile(_yamlFilePath);
+            });
+            return base.CreateHost(builder);
+        }
+    }
+}
