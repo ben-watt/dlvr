40ad0c76cefebf1c40b668a0dddbd9e2c2c7dec8
ben watt <ben.watt@sorted.com>
2021-07-08 22:38:50 +0100

    Add background publisher

---
+++ src/core/InMemoryStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace messaging_sidecar
+{
+    public class InMemoryStore
+    {
+        private readonly Channel<Message> _messages = Channel.CreateUnbounded<Message>();
+
+        public async Task Add(Message message)
+        {
+            await _messages.Writer.WriteAsync(message);
+        }
+
+        public async IAsyncEnumerable<Message> GetMessages()
+        {
+            await foreach(var message in _messages.Reader.ReadAllAsync())
+            {
+                yield return message;
+            }
+        }
+    }
+}
