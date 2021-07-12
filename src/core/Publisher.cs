d9f5ec2b7d39fbf77262563b6334b43f01fd2344
Ben Watt <bmwatt101@gmail.com>
2021-07-12 22:23:20 +0100

    Create background publisher to handle the messages (#1)
    * Add background publisher
    
    * Update docker file to run tests in the build pipeline
    
    * Re-point the docker compose file to the right dir
    
    * On pull request run a automated build
    
    Co-authored-by: ben watt <ben.watt@sorted.com>

---
+++ src/core/Publisher.cs
@@ -0,0 +1,45 @@
+﻿using messaging_sidecar_interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace messaging_sidecar
+{
+    public class Publisher
+    {
+        private readonly ILogger<Publisher> _logger;
+        private readonly IFactory<IPublish> _publisherFactory;
+
+        public Publisher(ILogger<Publisher> logger, IFactory<IPublish> publisherFactory)
+        {
+            _logger = logger;
+            _publisherFactory = publisherFactory;
+        }
+
+        public async Task<ProcessResponse> Publish(string publisherName, string topic, string body)
+        {
+            var publisher = _publisherFactory?.Create(publisherName);
+            if (publisher is null)
+            {
+                _logger.LogInformation("Unable to find {0} with the name: '{1}'", typeof(IPublish), publisherName);
+                return ProcessResponse.Failed;
+            }
+
+            var response = await publisher.Publish(topic, body);
+
+            if (response == ProcessResponse.Success)
+            {
+                _logger.LogInformation("Published message to topic: {0}", topic);
+                return ProcessResponse.Success;
+            }
+            else
+            {
+                _logger.LogInformation("Failed to publish message to topic: {0}", topic);
+                return ProcessResponse.Failed;
+            }
+        }
+    }
+}
